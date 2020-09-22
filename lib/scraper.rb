a3a67715a8537de7dfccab975c091af073ab285c
Brenda Ferrufino <ferrufinob@gmail.com>
2020-09-22 01:37:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,19 +17,20 @@ class Scraper
     # end
   end
   
-  
+  def get_courses
+     self.get_page.css(".post")
+  end
   
   
   def print_courses
-    self.get_page.css(".post")
-    # self.make_courses
-    # Course.all.each do |course|
-    #   if course.title && course.title != ""
-    #     puts "Title: #{course.title}"
-    #     puts "  Schedule: #{course.schedule}"
-    #     puts "  Description: #{course.description}"
-    #   end
-    # end
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
   end
   
 end
